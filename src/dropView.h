6b2c6cf05f8a819500387ca6b482004038e1dd04
Michael Votaw <mjvotaw@gmail.com>
2014-04-21 17:02:51 -0500

    Added request.js to fulfill some Less dependency.

---
+++ src/dropView.h
@@ -0,0 +1,13 @@
+//
+//  dropView.h
+//  LESSCompile
+//
+//  Created by Michael on 4/21/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface dropView : UIView
+
+@end
---
+++ src/dropView.m
@@ -0,0 +1,31 @@
+//
+//  dropView.m
+//  LESSCompile
+//
+//  Created by Michael on 4/21/14.
+//
+//
+
+#import "dropView.h"
+
+@implementation dropView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
